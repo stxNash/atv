9f68acc73dc50530bddff7ef3956eb79f71b16d7
stxNash <145800502+stxNash@users.noreply.github.com>
2023-10-06 14:43:16 -0300

    Add files via upload

---
+++ tabela3/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    int a = 7;
+    int b = a - 6;
+    int [] v =  new int [10];
+    while (b < a)
+    {
+        v [b] = b + a;
+        b = b + 2;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (v[i] != 0) 
+        {
+            Console.WriteLine(v[i]);
+        }
+    }
+}
