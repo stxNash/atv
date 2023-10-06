f7478dd257dbaf0ea5a18ecaaab6fde184d0d75e
stxNash <145800502+stxNash@users.noreply.github.com>
2023-10-06 14:46:24 -0300

    Add files via upload

---
+++ problema5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        decimal valorFuturo = 7390.61M; 
+        decimal taxaJuros = 2.00M / 100;  
+
+        decimal valorPresente = valorFuturo / (decimal)Math.Pow(1 + (double)taxaJuros, 3); 
+        
+
+        Console.WriteLine($"Valor Presente necessário para obter R$ {valorFuturo:F2} em 3 anos somados com a taxa de juros: R$ {valorPresente:F2}");
+    }
+}
