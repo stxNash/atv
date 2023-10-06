e77800d024c26928101f1309c597099911c60397
stxNash <145800502+stxNash@users.noreply.github.com>
2023-10-06 14:43:54 -0300

    Add files via upload

---
+++ problema1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+
+{
+    static void Main()
+
+    {
+        decimal valorPresente = 1000.00M; 
+
+        decimal taxaJuros = 5.30M / 100;  
+
+        int periodoMes = 10;               
+
+        decimal valorFuturo = valorPresente * (decimal)Math.Pow(1 + (double)taxaJuros, periodoMes);
+
+        Console.WriteLine($"Valor Futuro após {periodoMes} meses: R$ {valorFuturo:F2}");
+    }
+    
+}
